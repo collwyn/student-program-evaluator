20283e89a20e2f69818e25918578a97f99d1e0d6
collwyn <pbase@att.net>
2025-04-20 12:31:42 -0400

    attempting to fix mongo timeout errors

--- server/server.js
+++ server/server.js
@@ -11,9 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
+// server.js - Update your mongoose.connect call
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  serverSelectionTimeoutMS: 30000, // Increase from default 10000ms to 30000ms
+  socketTimeoutMS: 45000,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err.message));
